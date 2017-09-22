c4771bf177ce38efa03f8287ca411f97651efcf5
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-09-22 11:37:25 +0200

    Fix bug when saving a checkbox value whose value was a number. lodash.set set an array value instead of an object

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,4 +1,4 @@
-import set from 'lodash.set';
+import setWith from 'lodash.setwith';
 
 import { SET_DATA, RESET_DATA } from './actions';
 
@@ -8,7 +8,12 @@ export default function stateReducer(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case SET_DATA:
-      return set({ ...state }, payload.key, payload.value);
+      return setWith(
+        { ...state },
+        payload.key,
+        payload.value,
+        nsValue => nsValue || {},
+      );
 
     case RESET_DATA:
       return initialState;
