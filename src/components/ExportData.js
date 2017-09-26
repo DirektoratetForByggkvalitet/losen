49eb6289fc843f311f8e2fc18ac988cd7690d09e
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-09-26 14:59:58 +0200

    Allow non a11y-emojis

--- src/components/ExportData.js
+++ src/components/ExportData.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { Textarea } from '../primitives/Input';
+import Button from '../primitives/Button';
 import { getData } from '../utils/selectors';
 
-function ExportData({ data }) {
-  const json = JSON.stringify(data, null, 2);
-  const lines = json.split(/\n/).length;
+class ExportData extends Component {
+  static propTypes = {
+    data: PropTypes.any,
+  }
 
-  return <Textarea rows={lines}>{json}</Textarea>;
+  static defaultProps = {
+    data: null,
+  }
+
+  state = { open: false }
+
+  selectText = () => this.textarea.select()
+  showExportData = () => this.setState({ open: true })
+
+  render() {
+    const { data } = this.props;
+    const { open } = this.state;
+
+    if (!open) {
+      return <Button onClick={this.showExportData}>GIMME DAT JSON 🤓</Button>;
+    }
+
+    const json = JSON.stringify(data, null, 2);
+    const lines = json.split(/\n/).length;
+
+    return (
+      <Textarea
+        rows={lines}
+        innerRef={textarea => this.textarea = textarea}
+        onClick={this.selectText}
+      >{json}</Textarea>
+    );
+  }
 }
-
-ExportData.propTypes = {
-  data: PropTypes.any,
-};
-
-ExportData.defaultProps = {
-  data: null,
-};
 
 const mapStateToProps = (state, { exporter }) => ({
   data: exporter(getData(state)),
