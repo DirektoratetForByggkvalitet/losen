5af32ff3a5c31ba28bf641a457846e9f2290f4f9
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-09-29 12:26:12 +0200

    Have textarea support infomration

--- src/web/components/blocks/Textarea.js
+++ src/web/components/blocks/Textarea.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Html from '../helper/Html';
 import { Textarea as StyledTextarea } from '../../primitives/Input';
+import ErrorIcon from '../graphics/ErrorIcon';
+import Information from '../../primitives/Information';
 
 export default class Textarea extends Component {
   static defaultProps = {
     placeholder: '',
     setData: () => {},
     currentValue: '',
-  }
+    information: '',
+  };
 
   static propTypes = {
     property: PropTypes.string.isRequired,
     setData: PropTypes.func,
     placeholder: PropTypes.string,
     currentValue: PropTypes.string,
-  }
+    information: PropTypes.string,
+  };
 
   handleChange = (e) => {
     const { property, setData } = this.props;
     const value = e.target.value;
 
     setData(property, value);
-  }
+  };
 
   render() {
-    const { placeholder, currentValue } = this.props;
+    const { placeholder, currentValue, information } = this.props;
 
     return (
-      <StyledTextarea
-        placeholder={placeholder}
-        value={currentValue}
-        onChange={this.handleChange}
-      />
+      <div>
+        <StyledTextarea
+          placeholder={placeholder}
+          value={currentValue}
+          onChange={this.handleChange}
+        />
+        {information && (
+          <Information>
+            <ErrorIcon triangleFill={'black'} />
+            <Html text={information} />
+          </Information>
+        )}
+      </div>
     );
   }
 }
