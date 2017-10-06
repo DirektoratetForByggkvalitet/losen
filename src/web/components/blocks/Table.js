424e2e57dc9179f16400fc3a290b8006797124ce
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-10-06 10:24:46 +0200

    Add table

--- src/web/components/blocks/Table.js
+++ src/web/components/blocks/Table.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import pick from 'lodash.pick';
 
-export default function Table() {
-  return <div>apekatt</div>;
+function TableHeader(props) {
+  return <th {...props} />;
 }
+
+function TableCell(props) {
+  return <td {...props} />;
+}
+
+export default function Table({ cells }) {
+  return (
+    <table>
+      {cells.map(row => (
+        <tr>
+          {row.map((cell) => {
+            const cellProps = pick(cell, ['colspan', 'rowspan']);
+            const Cell = cell.type === 'Heading' ? TableHeader : TableCell;
+
+            return <Cell {...cellProps}>{cell.text}</Cell>;
+          })}
+        </tr>
+      ))}
+    </table>
+  );
+}
+
+Table.propTypes = {
+  cells: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.oneOf(['Heading', 'Cell']).isRequired,
+        text: PropTypes.string.isRequired,
+        colspan: PropTypes.number,
+        rowspan: PropTypes.number,
+      }),
+    ),
+  ).isRequired,
+};
