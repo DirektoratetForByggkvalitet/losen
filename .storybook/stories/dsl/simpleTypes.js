a3f21caf61d28bce598da519ca484c970215157c
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-10-11 13:03:22 +0200

    Update spelling

--- .storybook/stories/dsl/simpleTypes.js
+++ .storybook/stories/dsl/simpleTypes.js
@@ -7,18 +7,20 @@ export default function DslSimpleTypes() {
     <div>
       <H1>Simple types in the DSL</H1>
       <p>
-        The simple types is the comparisons, existence checks and negations that give power
-        to the expression. Each of the types is described in detail below.
+        The simple types is the comparisons, existence checks and negations that
+        give power to the expression. Each of the types is described in detail
+        below.
       </p>
 
       <H2>Field reference as expression value</H2>
       <p>
-        The comparisons can reference fields for the value to compara agains by using a "field
-        reference". The field reference is an object with a field property, like this:
+        The comparisons can reference fields for the value to compare agains by
+        using a "field reference". The field reference is an object with a field
+        property, like this:
       </p>
 
       <H3>Example of comparing against a scalar value</H3>
-      <pre>{'{ field: \'some.field\', operator: \'gt\', value: 4 }'}</pre>
+      <pre>{"{ field: 'some.field', operator: 'gt', value: 4 }"}</pre>
 
       <H3>Example of comparing agains another field</H3>
       <pre>{`{
@@ -34,39 +36,48 @@ export default function DslSimpleTypes() {
       <H2>The expression types</H2>
       <H3>gt</H3>
       <p>Expression describing a greater than comparison.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'gt\', value: 3 }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'gt', value: 3 }"}</pre>
 
       <H3>lt</H3>
       <p>Expression describing a less than comparison.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'lt\', value: 3 }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'lt', value: 3 }"}</pre>
 
       <H3>gte</H3>
       <p>Expression describing a greater than or equal comparison.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'gte\', value: 3 }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'gte', value: 3 }"}</pre>
 
       <H3>lte</H3>
       <p>Expression describing a less than or equal comparison.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'lte\', value: 3 }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'lte', value: 3 }"}</pre>
 
       <H3>eq</H3>
       <p>Expression describing a equality check.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'eq\', value: \'apekatt\' }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'eq', value: 'apekatt' }"}</pre>
 
       <H3>neq</H3>
       <p>Expression describing a not equal comparison.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'eq\', value: true }'}</pre>
+      <pre>{"{ field: 'some.field', operartor: 'eq', value: true }"}</pre>
 
       <H3>between</H3>
-      <p>Expression describing a range check, the value being between two values.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'between\', value: [3, 6] }'}</pre>
+      <p>
+        Expression describing a range check, the value being between two values.
+      </p>
+      <pre>
+        {"{ field: 'some.field', operartor: 'between', value: [3, 6] }"}
+      </pre>
 
       <H3>required</H3>
-      <p>Expression describing a required value, meaning that the field exists.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'required\' }'}</pre>
+      <p>
+        Expression describing a required value, meaning that the field exists.
+      </p>
+      <pre>{"{ field: 'some.field', operartor: 'required' }"}</pre>
 
       <H3>not</H3>
-      <p>Expression describing a check for a property that must have a falsy value.</p>
-      <pre>{'{ field: \'some.field\', operartor: \'not\' }'}</pre>
+      <p>
+        Expression describing a check for a property that must have a falsy
+        value.
+      </p>
+      <pre>{"{ field: 'some.field', operartor: 'not' }"}</pre>
     </div>
   );
 }
