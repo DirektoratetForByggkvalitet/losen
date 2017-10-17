47ba19a5d15013d6cb170949489eefc465dbfe3f
Kristoffer Brabrand <kristoffer@brabrand.no>
2017-10-17 08:59:06 +0200

    Fix option heading mapping for radio/select too. Closes #99

--- src/web/components/Summary/value/index.js
+++ src/web/components/Summary/value/index.js
@@ -8,17 +8,22 @@ export default function Value({ value, node }) {
     return <StyledValue missing>* Må fylles ut</StyledValue>;
   }
 
-  if (typeof value === 'object') {
-    const values = Object
-      .keys(value)
-      .filter(key => value[key])
-      .map((val) => {
-        const option = (node.options || []).find(
-          ({ value: optionValue }) => optionValue === val,
-        ) || {};
+  if (node.options) {
+    let values;
+    if (typeof value === 'object') {
+      values = Object.keys(value).filter(key => value[key]);
+    } else {
+      values = [value];
+    }
 
-        return option.heading || option.text || val;
-      });
+    // Map the values to option heading/text for the option
+    values = values.map((val) => {
+      const option = (node.options || []).find(
+        ({ value: optionValue }) => optionValue === val,
+      ) || {};
+
+      return option.heading || option.text || val;
+    });
 
     if (!node.optional && !values.length) {
       return <StyledValue missing>* Må fylles ut</StyledValue>;
@@ -29,10 +34,12 @@ export default function Value({ value, node }) {
     const commaSeparated = values.slice(0, -1);
     const lastOne = values.slice(-1)[0];
 
+    // If only one value selected
     if (!commaSeparated.length) {
       return <StyledValue>{lastOne}</StyledValue>;
     }
 
+    // If more than one selected
     return <StyledValue>{commaSeparated.join(', ')} og {lastOne}.</StyledValue>;
   }
 
