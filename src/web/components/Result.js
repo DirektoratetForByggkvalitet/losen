b3214d37bc77581f6c8d113f27b52ff5cd46f1c0
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-12-01 15:58:52 +0100

    Add summaryTitle

--- src/web/components/Result.js
+++ src/web/components/Result.js
@@ -18,6 +18,8 @@ import Export from '../primitives/Export';
 function Result({
   children = [],
   debug,
+  errorheading,
+  errorlead,
   errorPages,
   exporter,
   heading,
@@ -26,9 +28,8 @@ function Result({
   previousPage,
   schema,
   setPage,
+  summaryTitle,
   title,
-  errorheading,
-  errorlead,
 }) {
   if (errorPages.length) {
     return (
@@ -42,6 +43,7 @@ function Result({
         title={title}
         errorheading={errorheading}
         errorlead={errorlead}
+        summaryTitle={summaryTitle}
       />
     );
   }
@@ -54,7 +56,7 @@ function Result({
       </Lead>
 
       <SpecificBlock>
-        <H1 small>{title}</H1>
+        {summaryTitle ? <H1 small>{summaryTitle}</H1> : <H1 small>{title}</H1>}
         <Summary errorPages={errorPages} setPage={setPage} pages={schema} />
       </SpecificBlock>
 
@@ -63,7 +65,9 @@ function Result({
       ))}
 
       <Export exporter={exporter}>
-        <MainButton type="button" onClick={() => window.print()}>Skriv ut</MainButton>
+        <MainButton type="button" onClick={() => window.print()}>
+          Skriv ut
+        </MainButton>
       </Export>
     </Main>
   );
@@ -82,6 +86,7 @@ Result.propTypes = {
   previousPage: PropTypes.func.isRequired,
   schema: PropTypes.arrayOf(PropTypes.object),
   setPage: PropTypes.func.isRequired,
+  summaryTitle: PropTypes.string,
   title: PropTypes.string,
 };
 
@@ -94,6 +99,7 @@ Result.defaultProps = {
   heading: 'Missing page heading',
   lead: '',
   schema: [],
+  summaryTitle: '',
   title: null,
 };
 
