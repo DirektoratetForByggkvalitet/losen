8dd13ba13c99782ed4991b9ed0db1173daef25a1
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-12-13 18:09:31 +0100

    Add unit to summary

--- src/web/components/Summary/value/index.js
+++ src/web/components/Summary/value/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Html from '../../helper/Html';
 
 import { Value as StyledValue } from '../../../primitives/Summary';
 
@@ -18,9 +19,8 @@ export default function Value({ value, node }) {
 
     // Map the values to option heading/text for the option
     values = values.map((val) => {
-      const option = (node.options || []).find(
-        ({ value: optionValue }) => optionValue === val,
-      ) || {};
+      const option =
+        (node.options || []).find(({ value: optionValue }) => optionValue === val) || {};
 
       return option.heading || option.text || val;
     });
@@ -40,10 +40,18 @@ export default function Value({ value, node }) {
     }
 
     // If more than one selected
-    return <StyledValue>{commaSeparated.join(', ')} og {lastOne}.</StyledValue>;
+    return (
+      <StyledValue>
+        {commaSeparated.join(', ')} og {lastOne}.
+      </StyledValue>
+    );
   }
 
-  return <StyledValue>{value}</StyledValue>;
+  return (
+    <StyledValue>
+      {value} {node.unit ? <Html inline text={node.unit} /> : null}
+    </StyledValue>
+  );
 }
 
 Value.propTypes = {
