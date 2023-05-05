580ad64f637cc0bdee4c5bf5973b603d0599ab0c
Joakim Bording <joakim@behalf.no>
2023-05-05 15:17:51 +0200

    Refactoring modal to add correct overflow scroll behaviour on small screens

---
+++ src/web/primitives/ModalBox.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import injectStyles from '../utils/inject-styles';
+
+const ModalBox = injectStyles(styled.div`
+  display: block;
+  max-width: 666px;
+  border: 1px solid #ccc;
+  background: #fff;
+  border-radius: 0;
+  padding: 40px 60px;
+  text-align: center;
+`);
+
+export default ModalBox;
