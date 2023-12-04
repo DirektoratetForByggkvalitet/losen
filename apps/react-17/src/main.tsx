7bcdfe1bfc8d00e7deb41a99a4a92a06c5e9e92e
Kristoffer Brabrand <kristoffer@brabrand.no>
2023-12-04 22:02:18 +0100

    Remove React.StrictMode in react 17 example

--- apps/react-17/src/main.tsx
+++ apps/react-17/src/main.tsx
@@ -1,9 +1,4 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>, document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
