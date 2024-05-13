6def4dee54f63d33100fdc87b857cf2c9a50c922
Kristoffer Brabrand <kristoffer@brabrand.no>
2024-05-13 12:44:46 +0200

    Fix button floating when export + print button shows (#192)

--- packages/losen/primitives/Export.ts
+++ packages/losen/primitives/Export.ts
@@ -3,7 +3,25 @@ import styled from "styled-components";
 import injectStyles from "../utils/inject-styles";
 
 export const Export = injectStyles(styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-gap: 8px;
+
+  @media screen and (min-width: 600px) {
+    flex-direction: row;
+  }
+
   @media print {
     display: none;
   }
 `);
+
+export const ExportButtonWrapper = injectStyles(styled.div`
+  display:flex;
+  flex-grow: 0;
+  flex-basis: 100%;
+
+  > * {
+    flex-basis: 100%;
+  }
+`)
